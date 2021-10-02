d1191ac5778b11fc60bdd0c28ac21aa5fed4ac2e
Mark Stanley <mstanley@technologist.com>
2021-10-02 09:28:21 -0400

    Add list of hardware commands from CommandList.ods to the CmdProcessor.ino Arduino sketch

--- Arduino software/CmdProcessor/CmdProcessor.ino
+++ Arduino software/CmdProcessor/CmdProcessor.ino
@@ -1,9 +1,11 @@
 
-#define szSerialWord 4    // Size in number of bytes
 size_t numBytesRead;
 
+#define szSerialWord 4    // Size in number of bytes
 uint32_t serialWord;                            // Serial word as a single 32 bits
 byte* serialWordArray = (byte *) &serialWord;   // Serial word as an array of bytes
+
+uint32_t spiWord;   // This holds the bit masked & shifted value from serialWord
 
 bool newCommandWord = false;
 
@@ -13,13 +15,14 @@ int spi_clock;
 int spi_select;
 
 
-// 32 bit Control Word Masking bits
+/*** 32 bit CONTROL WORD and MASKING BITS ***/
 unsigned short Data;        // 16 bits
 byte Command;
 byte CommandBits = 0xF8;    // Mask to select 5 bits of Command from serialWord[1]
 byte Address;
 byte AddressBits = 0x07;    // Mask to select 3 bits of Address from serialWord[1]
 byte CommandFlag = 0xFF;    // Byte pattern to identify a Control Word
+
 
 /*********** HARDWARE DEFINITIONS ***********/
 // Pin assignments for Version 2 of the circuit board
@@ -45,33 +48,46 @@ int PLL_MUX_MISO = A2;
 int SPI_CLOCK = 10;
 
 // Addresses for selecting the various hardware ICs.
-const int Attenuator = 0;
-short AttenuatorBits = 0x007F;    // Mask to use 7 bits of Embedded Data
-const int LO1 = 1;
-const int LO2 = 2;
-short LO2Bits = 0x0F;             // Mask to use 4 bits of Embedded Data
-const int LO3 = 3;
-short LO3Bits = 0x0F;             // Mask to use 4 bits of Embedded Data
-const int RefClock = 4;
-const int PLL_Mux = 5;
-const int LED = 6;
+const int Attenuator  = 0;
+short ATTEN_Bits = 0x007F;      // 7 bits of Embedded Data
+const int LO1         = 1;
+const int LO2         = 2;
+short LO2_Bits     = 0x0F;      // 4 bits of Embedded Data
+const int LO3         = 3;
+short LO3_Bits     = 0x0F;      // 4 bits of Embedded Data
+const int RefClock    = 4;
+const int PLL_Mux     = 5;
+const int Reserved    = 6;
+const int LED         = 7;
 
 // Command names for operating LO1, LO2 and LO3
-const int RF_off = 1;
-const int neg_4dBm = 2;
-const int neg_1dBm = 3;
-const int pos_2dBm = 4;
-const int pos_5dBm = 5;
+const int General   = 0;
+const int RF_off    = 1;
+const int neg_4dBm  = 2;
+const int neg_1dBm  = 3;
+const int pos_2dBm  = 4;
+const int pos_5dBm  = 5;
 const int No_change = 6;
 
 // Command names for operating the reference oscillators
-const int all_off = 0;
+const int all_off  = 0;
 const int sel_ref1 = 1;
 const int sel_ref2 = 2;
 
+// Command names for operating the PLL Mux
+const int TRI_STATE    =  0;
+const int D_VDD        =  1;
+const int D_GND        =  2;
+const int R_DIV_OUT    =  3;
+const int N_DIV_OUT    =  4;
+const int ANA_LOCK_DET =  5;
+const int DIG_LOCK_DET =  6;
+const int SYNC_INPUT   =  7;
+const int READ_REG_6   = 12;  // 0xC
+
 // Command names for operating the Arduino builtin LED
 const int LED_off = 0;
-const int LED_on = 1;
+const int LED_on  = 1;
 
 /*********** HARDWARE DEFINITIONS END *******/
 
@@ -83,6 +99,9 @@ const int LED_on = 1;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(miso_pin, OUTPUT);
+  pinMode(mosi_pin, INPUT);
+  pinMode(spi_clock, OUTPUT);
+  pinMode(spi_select, OUTPUT);
 
   Serial.setTimeout(200);
   Serial.begin(2000000);
@@ -91,25 +110,23 @@ void setup() {
 
 
 
-void loop() {
-  while (Serial.available()) {
+void loop()
+{
+  while (Serial.available())
+  {
     numBytesRead = Serial.readBytes(serialWordArray, szSerialWord);
-
-    // PARSING THE SERIAL WORD into Data, Command and Address
-    if (numBytesRead == szSerialWord) {
-      if (serialWordArray[0] == CommandFlag) {
-        Data = serialWord >> 16;
-        Command = (serialWordArray[1] & CommandBits) >> 3;
-        Address = serialWordArray[1] & AddressBits;
-        newCommandWord = true;
-      }
-      else {
-        // Write the Direct Data Words to the LO's
-        spi_write();
-
-        // TODO: Buffer Direct Data Words and
-        // simultaneously(?) program the chips.
-      }
+    /* Check for a Command Word and parse into Data, Command and Address */
+    if (serialWordArray[0] == CommandFlag)
+    {
+      Data = serialWord >> 16;
+      Command = (serialWordArray[1] & CommandBits) >> 3;
+      Address = serialWordArray[1] & AddressBits;
+      newCommandWord = true;
+    }
+    else if (numBytesRead > 0)
+    {
+      /* TODO: Buffer Directdata Words */
+      spi_write();    // Write Directdata Word to the selected device
     }
   } // End While
 
@@ -117,7 +134,7 @@ void loop() {
   // Hardware selection and operations.
   if (newCommandWord) {
 
-    // For testing only - Remove at your earliest convenience.
+    // This line is for testing only - Remove at your earliest convenience.
     printCommandWord();
 
     // Start by selecting the device that you want to control. Then under
@@ -127,7 +144,8 @@ void loop() {
         mosi_pin = ATTEN_MOSI;
         spi_clock = ATTEN_CLOCK;
         spi_select = ATTEN_SEL;
-        Data &= AttenuatorBits;
+        Data &= ATTEN_Bits;
+        
         Serial.print("Digital Attenuator selected and its command is set to ");
         Serial.println(Data, HEX);
         break;
@@ -153,11 +171,8 @@ void loop() {
           case pos_5dBm:
             Serial.println("Setting LO1 to +5dBm absolute");
             break;
-          case No_change:
-            Serial.println("Setting LO1 to NO POWER CHANGE");
-            break;
           default:
-            // turn off LO1 RFout
+            Serial.println("Setting LO1 to NO POWER CHANGE");
             break;
         }
         break;
@@ -167,7 +182,7 @@ void loop() {
         mosi_pin = LO2_MOSI;
         spi_clock = SPI_CLOCK;
         spi_select = LO2_SEL;
-        Data &= LO2Bits;
+        Data &= LO2_Bits;
         Serial.print("LO2 Data = ");
         Serial.println(Data, HEX);
         switch (Command) {
@@ -186,11 +201,8 @@ void loop() {
           case pos_5dBm:
             Serial.println("Setting LO2 to +5dBm absolute");
             break;
-          case No_change:
-            Serial.println("Setting LO2 to NO POWER CHANGE");
-            break;
           default:
-            // turn off LO2 RFout
+            Serial.println("Setting LO2 to NO POWER CHANGE");
             break;
         }
         break;
@@ -200,7 +212,7 @@ void loop() {
         mosi_pin = LO3_MOSI;
         spi_clock = SPI_CLOCK;
         spi_select = LO3_SEL;
-        Data &= LO3Bits;        // Data bitmask for LO3 - TODO: Create a constant for the mask
+        Data &= LO3_Bits;
         Serial.print("LO3 Data = ");
         Serial.println(Data, HEX);
         switch (Command) {
@@ -219,11 +231,8 @@ void loop() {
           case pos_5dBm:
             Serial.println("Setting LO3 to +5dBm absolute");
             break;
-          case No_change:
-            Serial.println("Setting LO3 to NO POWER CHANGE");
-            break;
           default:
-            // turn off LO3 RFout
+            Serial.println("Setting LO3 to NO POWER CHANGE");
             break;
         }
         break;
@@ -244,16 +253,28 @@ void loop() {
           default:
             digitalWrite(REF1_SEL, LOW);
             digitalWrite(REF2_SEL, LOW);
-            Serial.println("Not using a Ref Clock");
+            Serial.println("Ref Clocks disabled");
             break;
         }
         break;
 
       // To use the PLL Mux you must first set the selection pin for one of the LO's.
+      // Once that's done you send the Command for READ_REG_6, do an spi_read() to
+      // get the register's contents, then send the Command TRI_STATE before moving
+      // on to the next LO.
       case PLL_Mux:
         Serial.println("PLL Mux address selected");
+        switch (Command) {
+          case READ_REG_6:
+            Serial.println("Set Mux pin to Read Register 6");
+            break;
+          default:
+            Serial.println("Set Mux pin to Tri-state output");
+            break;
+        }
         // TODO: Add spi_read()
         break;
+
 
       // Not part of the Interface Standard - For in-house testing only.
       case LED:
@@ -272,7 +293,9 @@ void loop() {
             break;
         }
         break;
+        
       default:
+        Serial.println("Invalid or unused hardware device address");
         break;
     }   /* End switch(Address) */
 
