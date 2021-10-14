fff470d354f62941bcd97797fc47d87209b4e6ca
Mark Stanley <mstanley@technologist.com>
2021-10-14 17:17:33 -0400

    Buffer serial reads and writes before communicating back and forth with the PC application

--- Arduino software/CmdProcessor/CmdProcessor.ino
+++ Arduino software/CmdProcessor/CmdProcessor.ino
@@ -13,41 +13,48 @@ SPISettings LO_SPI_Config(SPISettings(20000000, MSBFIRST, SPI_MODE0));
 
 #include "max2871.h"
 #include "adf4356.h"
-#include "hw_cmd_lists.h"       // Defined Commands IAW Interface Standard III
-
-size_t numBytesRead;
-
-#define szSerialWord 4    // Size in number of bytes
-uint32_t serialWord;                            // Serial word as a single 32 bits
-byte* serialWordArray = (byte *) &serialWord;   // Serial word as an array of bytes
+#include "hw_cmd_lists.h"  // Defined Commands IAW Interface Standard III
 
 
-/* For any given LO2/3 command there will be a 4 byte Instruction Word
-   (more specifically bits <20:16>) that informs the Arduino how many
-   32 bit Words are to follow.  */
-byte numDataWords; // See case LO2/3 below for masking of Data & LOx_Bits
+/*           Serial Word with Command Flag:
+    ________________________________________________
+   [       16 Bit        | 5 bit  | 3 b |   8 bit   ]
+   [       Embedded      | Instr- |Addr.|  Command  ]  NOTE: Command Flag
+   [       Data          | uction |     |   Flag    ]        always = 0xFF
+   [---------------------+--------+-----+-----------]
+   [ xxxx xxxx xxxx xxxx | xxxx x | xxx | 1111 1111 ]
+*/
+#define numBytesInSerialWord 4
+uint32_t serialWord;                         // Serial Word as 32 bits
+byte* serialWordBytes = (byte*)&serialWord;  // Serial Word as a byte array
 
-/* Max block size is 128 bytes and each Word contains 4 bytes.
-   So the max number of Data Words can be 32.  */
-const byte maxNumDataWords = 32;
-uint32_t dataWordArray[maxNumDataWords];
 
-// Indicates that a new Command has been sent to the Arduino
-// as denoted by a Command Flag, 0xFF, having been received.
+/* Several of the LO2/3 commands have an extra 6 bits of Embedded Data <21:16>
+   that tells the Arduino how many 32 bit Words are to follow. */
+int16_t wordCount;  // See 'case LO2:' below for masking of Data & LOx_Bits
+int16_t wordIndex;
+
+/* Max block size is 128 bytes and because each Data Word
+   contains 4 bytes the maximum number of Data Words is 32. */
+const byte szArrayWordBuff = 8;
+uint32_t dataWordBuf[szArrayWordBuff];
+uint16_t* dataWordBufInts = (uint16_t*)&dataWordBuf;
+
+// Command Flag 0xFF indicates that a new instruction was received by the Arduino
 bool newCommandReceived = false;
 
 /* Move this into a struct
    All the values required by the spi_write() command */
-int miso_pin;       // Arduino pin for reading Reg6 or A2D's
-int mosi_pin;       // Set this equal to the pin number of the device to be written to
+int miso_pin;  // Read values from an external A2D chip and Mux pin of LO's
+int mosi_pin;  // Set to write register values to the LO's or the Attenuator
 int spi_clock;
-int spi_select;     // Selects the device to be written to
-uint32_t spiWord;   // Holds the register contents to be written to the selected device
+int spi_select;    // Selects the device to be written to
+uint32_t spiWord;  // Holds the register contents to be written to the selected device
 
 
-/*** 32 bit SERIAL WORD SPLITS ***/
-uint16_t Data;            // 16 bits
-uint32_t Data32;          // Needed for bit shifting and masking with LO registers
+/*** Parsed values from the incoming 32 bit serial word ***/
+uint16_t Data;    // 16 bits
+uint32_t Data32;  // Needed for bit shifting and masking with LO registers
 byte Command;
 byte CommandBits = 0xF8;  // Mask to select 5 bits of Command from serialWord[1]
 byte Address;
@@ -73,18 +80,18 @@ const int SPI_CLOCK_PIN = 10;  // Common SPI clock for LO's and Attenuator
 
 
 // Addresses for selecting the various hardware ICs
-const int Attenuator    = 0;
-const int LO1_addr      = 1;
-const int LO2_addr      = 2;
-const int LO3_addr      = 3;
-const int RefClock      = 4;
-const int PLL_Mux       = 5;
-const int Reserved      = 6;
-const int LED           = 7;
+const int Attenuator = 0;
+const int LO1_addr   = 1;
+const int LO2_addr   = 2;
+const int LO3_addr   = 3;
+const int RefClock   = 4;
+const int PLL_Mux    = 5;
+//                   = 6;
+const int LED        = 7;
 
 
 // BitMask for programming the registers of the Attenuator IC
-const short ATTEN_Data_Mask     = 0x7F;      // 7 bits of Embedded Data
+const short ATTEN_Data_Mask = 0x7F;  // 7 bits of Embedded Data
 
 
 
@@ -97,13 +104,10 @@ MAX2871_LO LO3 = MAX2871_LO();
 MAX2871_LO* LO;  // Allows me to write one set of code for LO2 and LO3
 
 
-// Set by Embedded Data bits <BT5:BT0> when an LO2 or 3 command has been received
-// Denotes how many General Purpose register updates will be following.
-uint16_t block_count = 0;
-
 // Decide if one of the LO's received a command to update a register
 bool writeToSPI;
 
+bool displayVersionInfo = true;
 
 
 void setup() {
@@ -115,46 +119,68 @@ void setup() {
 
   Serial.setTimeout(200);
   Serial.begin(2000000);
-  Serial.println("\nWelcome to Vapor Ware\n");
 
-  writeToSPI = true;  // Assume we'll get a command that requires programming a chip...
+  writeToSPI = true;  // This goes false for commands that don't program a chip
+
 }
 
 
+uint16_t idx;
+uint16_t analog;
 
-void loop()
-{
+void loop() {
+  if (displayVersionInfo) {
+    Serial.println("\n Welcome to WN2A Spectrum Analyzer CmdProcessor \n 10/10/2021 2Mbaud");
+    displayVersionInfo = false;
+  }
+
   while (Serial.available())
   {
-    numBytesRead = Serial.readBytes(serialWordArray, szSerialWord);
-    /* Check for a Command Word and parse into Data, Command and Address */
-    if (serialWordArray[0] == CommandFlag)
+    Serial.readBytes(serialWordBytes, numBytesInSerialWord);
+    // If a General Command is received, then...
+    if (serialWordBytes[0] != CommandFlag)
     {
-      Data = (uint16_t)(serialWord >> 16);
-      Command = (serialWordArray[1] & CommandBits) >> 3;
-      Address = serialWordArray[1] & AddressBits;
-      newCommandReceived = true;
+      /* If a request for buffering data is received, then... */
+      if (wordIndex < wordCount) {
+        idx = wordIndex % szArrayWordBuff;
+//        dataWordBuf[idx] = serialWord;
+//        Serial.print(" Buf[");
+        Serial.println(wordIndex++);
+//        Serial.print("] = ");
+//        Serial.println(dataWordBuf[idx], HEX);
+
+//        wordIndex++;
+
+//        if ((wordIndex % szArrayWordBuff) == 0) {
+//          for (int i = 0; i < szArrayWordBuff; i++) {
+//            spi_write(dataWordBuf[i]);
+//            delay(1);   // bullshit delay between programming a register and readAnalog
+//            dataWordBufInts[i] = analogRead(A0);      // if this were the 315 LogAmp
+//          }
+//          for (int i = 0; i < szArrayWordBuff; i++) {
+//            Serial.print("Analog 16 bit value = ");
+//            Serial.println(dataWordBufInts[i]);
+//          }
+//          wordIndex = 0;
+//        }
+      }
     }
+    /* If a Command Word is found then parse into Data, Command and Address */
     else
     {
-      /* TODO: Buffer Directdata Words */
-      block_count = numDataWords;
-      if (block_count > 0) {
-        block_count--;          // decrement block_count
-        dataWordArray[block_count] = serialWord;
-        numDataWords--;   // decrement
-      }
-      if (numDataWords == 0) {
-      }
-      //      spi_write();    // Write Directdata Word to the selected device
+      Data = (uint16_t)(serialWord >> 16);
+      Command = (serialWordBytes[1] & CommandBits) >> 3;
+      Address = serialWordBytes[1] & AddressBits;
+      newCommandReceived = true;
     }
-  } // End While
+  }  // End While
 
 
-  // Hardware selection and operations.
+  /* Hardware selection and operations. This is where the processing of
+     Specific commands occurs. */
   if (newCommandReceived) {
-    // Start by selecting the device that you want to control. Then under
-    // each device you can select the operation that you want to perform.
+    /* Start by selecting the device that you want to control. Then under
+       each device you can select the operation that you want to perform. */
     switch (Address) {
       case Attenuator:
         mosi_pin = ATTEN_MOSI;
@@ -162,9 +188,7 @@ void loop()
         spi_select = ATTEN_SEL;
         Data &= ATTEN_Data_Mask;
         spiWord = ((uint32_t)Data << 16) | 0xFF;
-        spi_write();
-        Serial.print("Digital Attenuator selected and commanded to set ");
-        Serial.println(spiWord, HEX);
+        spi_write(spiWord);
         break;
 
       case LO1_addr:
@@ -174,12 +198,12 @@ void loop()
         Data32 = ((uint32_t)Data << 4); /* Needed to align <N16:N1> with R[0]<DB19:DB4> */
         switch (Command) {
           case RF_off:
-            LO1.Curr.R[6] =  LO1.Curr.R[6] & LO1.RFpower_off;
+            LO1.Curr.R[6] = LO1.Curr.R[6] & LO1.RFpower_off;
             spiWord = LO1.Curr.R[6];
             break;
           // Set the RFoutB power level, RFoutA is not used and can be disabled.
           case neg_4dBm:
-            LO1.Curr.R[6] = (LO1.Curr.R[6] & LO1.Power_Level_Mask) | LO1.neg4dBm; // neg4dBm is only for documentation
+            LO1.Curr.R[6] = (LO1.Curr.R[6] & LO1.Power_Level_Mask) | LO1.neg4dBm;  // neg4dBm is only for documentation
             spiWord = LO1.Curr.R[6];
             break;
           case neg_1dBm:
@@ -195,19 +219,19 @@ void loop()
             spiWord = LO1.Curr.R[6];
             break;
           case Mux_TRI:
-            LO1.Curr.R[4] = (LO1.Curr.R[4] & LO1.Mux_Set_TRI); // Turns on Tristate
+            LO1.Curr.R[4] = (LO1.Curr.R[4] & LO1.Mux_Set_TRI);  // Turns on Tristate
             spiWord = LO1.Curr.R[4];
             break;
           case Mux_DLD:
-            LO1.Curr.R[4] = LO1.Curr.R[4] | LO1.Mux_Set_DLD; // Set MuxOut to Dig. Lock Det.
+            LO1.Curr.R[4] = LO1.Curr.R[4] | LO1.Mux_Set_DLD;  // Set MuxOut to Dig. Lock Det.
             spiWord = LO1.Curr.R[4];
             break;
           default:
-            writeToSPI = false;    // Do not write to SPI. No commands were received
+            writeToSPI = false;  // Do not write to SPI. No commands were received
             break;
         }
         if (writeToSPI) {
-          spi_write();
+          spi_write(spiWord);
         }
         writeToSPI = true;  // Reset for next incoming serial command
         getLOstatus(LO1);
@@ -218,7 +242,7 @@ void loop()
         LO = &LO2;
         mosi_pin = LO2_MOSI;
         spi_select = LO2_SEL;
-        // NOTE: break is missing so the code falls through to 'case LO3_addr'
+      // NOTE: break is missing so the code falls through to 'case LO3_addr'
       case LO3_addr:
         /* Making LO3 active */
         if (Address = LO3_addr) {
@@ -228,15 +252,17 @@ void loop()
         }
         // Set the remainder of the SPI pins
         spi_clock = SPI_CLOCK_PIN;
-        Data &= LO->Data_Mask;      // Parse the incoming serial Embedded Data
-        numDataWords = (byte)Data;
+        wordCount = Data;           // Number of frequencies points to be read
+        Serial.print("word count = ");
+        Serial.println(wordCount);
         switch (Command) {
           case RF_off:
-            LO->Curr.R[4] =  LO->Curr.R[4] & LO->RFpower_off;
+            LO->Curr.R[4] = LO->Curr.R[4] & LO->RFpower_off;
             spiWord = LO->Curr.R[4];
             break;
           case neg_4dBm:
-            LO->Curr.R[4] = (LO->Curr.R[4] & LO->Power_Level_Mask) | LO->neg4dBm; // neg4dBm is only for documentation
+            // ' | LO->neg4dBm' does nothing and is only for documentation
+            LO->Curr.R[4] = (LO->Curr.R[4] & LO->Power_Level_Mask) | LO->neg4dBm;
             spiWord = LO->Curr.R[4];
             break;
           case neg_1dBm:
@@ -252,19 +278,19 @@ void loop()
             spiWord = LO->Curr.R[4];
             break;
           case Mux_TRI:
-            LO->Curr.R[2] = LO->Curr.R[2] & LO->Mux_Set_TRI; // Set MuxOut to Tristate
+            LO->Curr.R[2] = LO->Curr.R[2] & LO->Mux_Set_TRI;  // Set MuxOut to Tristate
             spiWord = LO->Curr.R[2];
             break;
           case Mux_DLD:
-            LO->Curr.R[2] = LO->Curr.R[2] | LO->Mux_Set_DLD; // Set MuxOut to Dig. Lock Det.
+            LO->Curr.R[2] = LO->Curr.R[2] | LO->Mux_Set_DLD;  // Set MuxOut to Dig. Lock Det.
             spiWord = LO->Curr.R[2];
             break;
           default:
-            writeToSPI = false;    // Do not write to SPI. No commands were received
+            writeToSPI = false;  // Do not write to SPI. No commands were received
             break;
         }
         if (writeToSPI) {
-          spi_write();
+          spi_write(spiWord);
         }
         writeToSPI = true;  // Reset for next incoming serial command
         getLOstatus(*LO);
@@ -287,20 +313,6 @@ void loop()
         }
         break;
 
-      // To use the PLL Mux you must first set the selection pin for one of the LO's.
-      // Once that's done you send the Command for READ_REG_6, do an spi_read() to
-      // get the register's contents, then send the Command TRI_STATE before moving
-      // on to the next LO.
-      case PLL_Mux:
-        switch (Command) {
-          default:
-            Serial.println("Shouldn't this be part of the LO's?");
-            break;
-        }
-
-
-
-      // Not part of the Interface Standard - For in-house testing only.
       case LED:
         switch (Command) {
           case LED_off:
@@ -310,7 +322,7 @@ void loop()
             digitalWrite(LED_BUILTIN, HIGH);
             break;
           case MSG_REQ:
-            Serial.println("Arduino CmdProcessor.ino 10 October 2021  2 Megabaud");
+            displayVersionInfo = true;
             break;
           default:
             break;
@@ -318,25 +330,24 @@ void loop()
         break;
 
       default:
-        Serial.println("Invalid device address");
+        Serial.print("Requested Address:");
+        Serial.print(Address);
+        Serial.println(" not found");
         break;
-    }   /* End switch(Address) */
 
-    /* Limit set by the size of the dataWordArray[maxNumDataWords] */
-    if (numDataWords > maxNumDataWords) {
-      numDataWords = maxNumDataWords;
-    }
+    } /* End switch(Address) */
+
     newCommandReceived = false;
-    
-  }  /* End newCommandReceived */
-}  /* End loop() */
+
+  } /* End newCommandReceived */
+} /* End loop() */
 
 
 // Program the selected device using shiftOut()
 /* This will be replaced by SPI.transfer() for the Ver 3 board */
-void spi_write() {
-  for (int i = 0; i < szSerialWord; i++) {
-    shiftOut(mosi_pin, spi_clock, MSBFIRST, spiWord);
+void spi_write(uint32_t spiData) {
+  for (int i = 0; i < numBytesInSerialWord; i++) {
+    shiftOut(mosi_pin, spi_clock, MSBFIRST, spiData);
   }
   digitalWrite(spi_select, HIGH);
   digitalWrite(spi_select, LOW);
@@ -345,7 +356,7 @@ void spi_write() {
 
 // This function is for development testing. Remove at production
 void getLOstatus(MAX2871_LO LO) {
-  for (int i=0; i<LO.Curr.numRegisters; i++) {
+  for (int i = 0; i < LO.Curr.numRegisters; i++) {
     Serial.print("R[");
     Serial.print(i);
     Serial.print("] = 0x");
@@ -356,7 +367,7 @@ void getLOstatus(MAX2871_LO LO) {
 
 // This function is for development testing. Remove at production
 void getLOstatus(ADF4356_LO LO) {
-  for (int i=0; i<LO.Curr.numRegisters; i++) {
+  for (int i = 0; i < LO.Curr.numRegisters; i++) {
     Serial.print("R[");
     Serial.print(i);
     Serial.print("] = 0x");
