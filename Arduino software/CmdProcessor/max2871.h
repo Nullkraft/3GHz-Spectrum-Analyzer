6202aeca9ef5b294326640d6ed0d0a4f40a70c7f
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2022-07-31 19:29:29 -0400

    Fix default register values to initialize MAX2871 to 80 MHz

--- Arduino software/CmdProcessor/max2871.h
+++ Arduino software/CmdProcessor/max2871.h
@@ -4,17 +4,17 @@
 #include <Arduino.h>    // Includes typedef for uint32_t
 
 
-/* Default register values for MAX2871 LO: Sets RFOout = 3945 MHz */
+/* Default register values for MAX2871 LO: Sets default RFOout = 80 MHz */
 typedef struct maxRegisters {
   static const byte numRegisters = 7;
   static const byte numProgrammableRegs = 5;
-  uint32_t Reg[numRegisters] = { 0x00419550,
-                                 0x2000FFE9,
-                                 0x58008042,    // Digital Lock detect ON
-                                 0xF8008003,
-                                 0x638FF1C4,
+  uint32_t Reg[numRegisters] = { 0x0026C9B0,
+                                 0x40017FF9,
+                                 0x98005F42,    // Digital Lock detect ON
+                                 0x00001F23,
+                                 0x63EFF1C4,    // RFout_A disabled
                                  0x00400005,    // Bit 18 is MSB for Digital Lock detect control
-                                 0x40009242     // Digital Lock detect OFF (Tri-state output)
+                                 0x80005F42     // Digital Lock detect OFF (Tri-state output)
                                };
 //  uint16_t* RLO2_as_int = (uint16_t*)RLO2;
 } max2871Registers;
