42b83bf32a741c685fc5869430b325e840d5d021
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-10-30 13:56:35 -0400

    Clarify comment

--- CmdProcessor/src/max2871.cpp
+++ CmdProcessor/src/max2871.cpp
@@ -5,14 +5,14 @@
    register 5.                                                  Document Version: 19-7106; Rev 4; 6/20
 */
 void MAX2871_LO::begin(uint8_t selectPin, bool first_init) {
-  spiWrite(Curr.Reg[5], selectPin);  // First we program LO2 Register 5
+  spiWrite(Curr.Reg[5], selectPin);   // First we program LO2 Register 5
   if (first_init) {
     delay(20);  // Only if it's our first time must we wait 20 mSec
   }
   for (int x = 4; x >= 0; x--) {
-    spiWrite(Curr.Reg[x], selectPin);  // and Lock Detect is enabled on the Mux pin
+    spiWrite(Curr.Reg[x], selectPin); // and Lock Detect is enabled on the Mux pin
   }
-  spiWrite(Curr.Reg[6], selectPin);  // Tri-stating the mux output disables LO2 lock detect
+  spiWrite(Curr.Reg[6], selectPin);   // Set mux tri-state to disable LO1 lock detect
 }
 
 void MAX2871_LO::set_NF_bits(uint32_t controlWord) {
