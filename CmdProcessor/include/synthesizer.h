4e2b4a7395e7145f984afb3e0ee3391f2d0037e7
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-11-10 20:11:25 -0500

    Add begin() and update() to the list of virtual functions

--- CmdProcessor/include/synthesizer.h
+++ CmdProcessor/include/synthesizer.h
@@ -5,8 +5,8 @@
 
 class LO {
   public:
-    // virtual void begin(uint32_t reg, uint8_t selectPin) = 0;
-    // virtual void update(uint32_t reg, uint8_t selectPin) = 0;
+    virtual void begin(uint8_t selectPin) = 0;
+    virtual void update(uint32_t reg, uint8_t selectPin) = 0;
     // Default value for controlWord to account for ADF4356
     virtual uint32_t Execute(byte commandIndex, uint32_t controlWord = 0) = 0;
     virtual ~LO() {}
